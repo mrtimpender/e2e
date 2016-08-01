50090d248c44f1fb91decefadce8c6baf94b66e8
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-01 11:19:42 -0600

    setting up client side dependencies

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/nm', express.static(__dirname + '/node_modules/'));
 app.use('/', routes);
 app.use('/users', users);
 
