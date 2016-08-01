40c710f1a9ee2c0c1e0ab7885d56216f44bb39fa
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-01 15:04:33 -0600

    lift oauth integration working well

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport')
+// var userQueries = require('../lib/database/users/userQueries')
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express Yeah!' });
   console.log(process.env.lyft_api_client_id);
   
 });
 
 router.get('/auth/lyft',
-  passport.authenticate('lyft', { scope: ['public','profile'] }
+  passport.authenticate('lyft', { scope: ['public','profile','rides.read'] }
 ));
  
-router.get('/lyftAuth', passport.authenticate('lyft', { failureRedirect: '/test' }),
-  function(req, res) {
+router.get('/lyftAuth', passport.authenticate('lyft', { failureRedirect: '/fail' }),
+  (req, res) => {
+  // create entry in user table with our token
+
     res.redirect('/');
 });
 
