238b8895c0015e9f2e77d875d260c6589a59987a
Tim Pender <mrtimpender@gmail.com>
2016-08-02 11:28:00 -0600

    adjusted migrations, added user_trips table

--- migrations/20160731171509_mig1.js
+++ migrations/20160731171509_mig1.js
@@ -2,9 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('e2e_users', function(table){
     table.increments('id').primary()
-    table.string('firstname')
-    table.string('lastname')
     table.string('e2e_username')
+    table.string('e2e_email')
     table.string('e2e_password')
     table.string('uber_key', 1000)
     table.string('lyft_key', 1000)
