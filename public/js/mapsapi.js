fd9c908b712b53a2d923c2bd404e7cda83220a5b
Jonathan Huxhold <jonathanhuxhold@Jonathans-MacBook-Pro.local>
2016-08-02 11:17:56 -0600

    adding geocoding functionality

--- public/js/mapsapi.js
+++ public/js/mapsapi.js
@@ -34,14 +34,20 @@ function initMap() {
   var directionsDisplay = new google.maps.DirectionsRenderer;
   var directionsService = new google.maps.DirectionsService;
   var trafficLayer = new google.maps.TrafficLayer();
+  var transitLayer = new google.maps.TransitLayer();
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
   });
   directionsDisplay.setMap(map);
   trafficLayer.setMap(null);
+  transitLayer.setMap(null);
 
   $('#showTraffic').click(function() {
     trafficLayer.getMap() == null ? trafficLayer.setMap(map) : trafficLayer.setMap(null);
+  })
+
+  $('#showTransit').click(function() {
+    transitLayer.getMap() == null ? transitLayer.setMap(map) : transitLayer.setMap(null);
   })
 
   calculateAndDisplayRoute(directionsService, directionsDisplay);
@@ -64,7 +70,22 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     } else {
       window.alert('Directions request failed due to ' + status);
     }
-  });
+  })
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ 'address': startPoint }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        console.log(parseFloat(results[0].geometry.location.lat()));
+        console.log(parseFloat(results[0].geometry.location.lng()));
+      };
+    })
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode({ 'address': endPoint }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        console.log(parseFloat(results[0].geometry.location.lat()));
+        console.log(parseFloat(results[0].geometry.location.lng()));
+      };
+    })
+
 }
 
 $('#getDirections').click(function(){
