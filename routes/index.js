1d1a1d3927b675ca02d4d73a0c694449d4b6ea75
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-02 16:51:11 -0600

    uber oauth working well

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport')
 var userQueries = require('../lib/database/users/userQueries')
+var db = require('../config/db')
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express Yeah!' });
@@ -13,6 +14,23 @@ router.get('/dashTest', (req, res, next) => {
 router.get('/allUsers', (req, res, next) => {
   userQueries.getAllUsers().then((resp) => res.json(resp))
 })
+
+
+router.get('/auth/uber', passport.authenticate('uber'));
+router.get('/auth/uber/callback', 
+  passport.authenticate('uber', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to auth completion OR dashboard
+    userQueries.getUserByUuid(req.session.passport.user.uuid).then((resp) => {
+      if(resp.length  === 1){
+        res.redirect('/dashTest')
+      } else {
+        res.redirect('/login/completeRegistration');
+      }
+    })
+  });
+
+
 router.get('/auth/lyft',
   passport.authenticate('lyft', { scope: ['public','profile','rides.read'] }
 ));
