05e5ea10b4af341cf49d5605d39e274d2c35f06b
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-02 11:25:20 -0600

    bookshelf and modelbase appear to be working TRUMP STEAKS

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var userQueries = require('../lib/database/users/userQueries')
+var db = require('../config/db')
+var modelBase = require('bookshelf-modelbase')(db.bookshelf)
+
 // login route
 router.get('/', (req, res, next) => {
   res.render('login/index')
@@ -10,9 +13,21 @@ router.get('/forgot_pass', (req, res, next) => {
   res.render('login/forgot_pass')
 })
 // register
-router.get('/register', (req, res, next) => {
-  res.render('login/register')
-})
+router.route('/register') 
+  .get((req, res, next) => {
+    res.render('login/register')
+  })
+  .post((req, res, next) => {    
+    modelBase.create({
+      e2e_username: req.body.e2e_username,
+      e2e_password: req.body.e2e_passowrd,
+      e2e_email: req.body.e2e_email
+    }, {
+      e2e_username: req.body.e2e_username
+    }).then((resp) => {
+      res.sendStatus(200)
+    })
+  })
 // after oauth register
 router.route('/completeRegistration')
   .get((req, res, next) => {
