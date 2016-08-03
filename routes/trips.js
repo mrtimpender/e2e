0d2a0967688ec8abed780f645b256fd4f9841c9c
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-03 14:57:40 -0600

    routing infastructure fixed

--- routes/trips.js
+++ routes/trips.js
@@ -1,14 +1,21 @@
+// trips route. Create CRUD routes for our trips. Look at datbase structure for format
+
 var express = require('express');
 var router = express.Router();
 var passport = require('passport')
 var googleMaps = require('../lib/google_maps_api/google_maps_api');
-/* GET home page. */
 
-router.get('/geocode', function(req, res, next) {
-  console.log(googleMaps);
-  
-  // res.json(req.body)
-  googleMaps.geocodeDirtyAddress('1341 niagara st denver').then((resp) => res.json(resp))
+router.route('/')
+  .get((req, res, next) => {
+    // load new trip tempate
+    res.render('trips/new_trip')
+  })
+  .post((req, res, next) => {
+    // create new trip route
+  })
+
+router.get('/geocodeTest', function(req, res, next) {
+    googleMaps.geocodeDirtyAddress('1341 niagara st denver').then((resp) => res.json(resp))
   // res.redirect('/dashboard/dashdash');
 });
 router.get('/auto', (req, res, next) => {
