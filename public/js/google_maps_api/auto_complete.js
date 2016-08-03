d210f273af049839089e98ff73881fdf5d5ac0fa
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-03 15:55:01 -0600

    map autocomplete field for new trip working

--- public/js/google_maps_api/auto_complete.js
+++ public/js/google_maps_api/auto_complete.js
@@ -9,8 +9,8 @@ function initMap() {
       var input = /** @type {!HTMLInputElement} */ (
             document.getElementById('pac-input'));
       var types = document.getElementById('type-selector');
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-      map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+      // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      // map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
       var autocomplete = new google.maps.places.Autocomplete(input);
       autocomplete.bindTo('bounds', map);
       var infowindow = new google.maps.InfoWindow();
