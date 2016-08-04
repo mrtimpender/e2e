95c84eb36460daf960a2b91fffdd81b99ab8aa9e
Jonathan Huxhold <jonathanhuxhold@Jonathans-MacBook-Pro.local>
2016-08-04 13:54:17 -0600

    adding form and modal to location route

---
+++ public/js/interactivity.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $('.modal-trigger').leanModal();
+
+
+  google.maps.event.addDomListener(window, 'load', function () {
+    var places = new google.maps.places.Autocomplete(document.getElementById('searchBoxField'));
+    google.maps.event.addListener(places, 'place_changed', function () {
+      var place = places.getPlace();
+      var address = place.formatted_address;
+    })
+  });
+
+})
