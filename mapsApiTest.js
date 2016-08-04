93c14e49ee950ca66afe30dfb23f15b84c97e235
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-03 21:55:23 -0600

    adding google maps directions api data

---
+++ mapsApiTest.js
@@ -0,0 +1,14 @@
+var console = require('better-console');
+
+
+var gmController = require('./lib/google_maps_api/googleMapsController')
+var gm = require('./lib/google_maps_api/google_maps_api')
+
+
+console.log(process.env.uber_api_client_secret);
+
+
+gm.getDirectionsFromAddresses('3358 Navajo St, Denver, CO 80211, USA' , '1644 Platte St, Denver, CO 80202, USA', 'driving').then((resp) => {
+  gmController.parseDirectionsData(resp)
+  // console.dir(JSON.stringify(resp))
+})
