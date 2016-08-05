07b985cef0c68fadabd88b0c60e57c96c4878d23
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-04 21:35:39 -0600

    uber location product data making it to database

--- controllers/uber_api/uberEstimateController.js
+++ controllers/uber_api/uberEstimateController.js
@@ -1,13 +1,17 @@
 var UberApi = require('./UberApi')
 var tripQueries = require('../database/trips/tripQueries')
+var uberProductModel = require('../database/uberModels/uberProductModel')
 
 exportMethods = {
   putEstimatesForUberTrip: (trip) => {
    console.log(trip);
-   UberApi.getAllProductsForLocation(trip).then((uberProductData) => {
-     console.log('UBER PRODUCT DATA', uberProductData);
+   UberApi.getAllProductsForLocation(trip.origin_lat, trip.origin_lng)
+    .then((uberProductData) => {
+      uberProductModel.createProduct(uberProductData, trip.origin_loc_id)
+        .then((response) => console.log(response))
+       console.log('UBER PRODUCT DATA', uberProductData);
      
-   })
+    })
    
   }
 }
