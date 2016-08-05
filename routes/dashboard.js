0d11cbfcdbf71ba799e4bf51dfe8c52aa1093ea9
Tim Pender <mrtimpender@gmail.com>
2016-08-05 00:26:10 -0600

    added user ids and other properties to the session variables

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var passport = require('passport')
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log('*********');
+    console.log(req.session);
+    console.log('*********');
   res.render('dash/index', {title: 'e2e | Dashboard'});
 })
 
