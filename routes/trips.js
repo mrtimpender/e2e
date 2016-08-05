d6fb6ee02d8d60af3210bfff43eb674023f8a0d1
Tim Pender <mrtimpender@gmail.com>
2016-08-05 17:45:40 -0600

    added links to the trips edit route

--- routes/trips.js
+++ routes/trips.js
@@ -8,9 +8,23 @@ var tripsController = require('../controllers/trips/tripsController')
 
 router.route('/')
   .get((req, res, next) => {
+  tripsController.returnTripsAndLocaitons().then((trips) => res.render('trips/my_trips', {
+    trips:trips.rows,
+    title: 'e2e | New Trip',
+    id: req.session.passport.user.id,
+    username: req.session.passport.user.username,
+    firstname: req.session.passport.user.firstname,
+    lastname: req.session.passport.user.lastname,
+    fullname: req.session.passport.user.firstname + " " + req.session.passport.user.lastname,
+    email: req.session.passport.user.email
+  }))
+})
+
+router.route('/new')
+  .get((req, res, next) => {
     // load new trip tempate
     res.render('trips/new_trip', {
-      title: 'e2e | Dashboard',
+      title: 'e2e | New Trip',
       id: req.session.passport.user.id,
       username: req.session.passport.user.username,
       firstname: req.session.passport.user.firstname,
@@ -23,28 +37,22 @@ router.route('/')
     // create new trip route
   })
 
-  router.route('/my_trips')
-    .get((req, res, next) => {
-    tripsController.returnTripsAndLocaitons().then((trips) => res.render('trips/my_trips', {trips:trips.rows}))
-  })
-
-
-// begin sluff
-router.route('/jon-test')
-  .get((req, res, next) => {
-    // load new trip tempate
-    res.render('trips/new_trip_jon')
-  })
-  .post((req, res, next) => {
-    // create new trip route
-  })
-
-router.get('/geocodeTest', function(req, res, next) {
-    googleMaps.geocodeDirtyAddress('1341 niagara st denver').then((resp) => res.json(resp))
-  // res.redirect('/dashboard/dashdash');
-});
-router.get('/auto', (req, res, next) => {
-  res.render('testing/autocomplete')
-})
+  // router.route('/edit/:id')
+  //   .get((req, res, next) => {
+  //
+  //
+  //     res.render('trips/new_trip', {
+  //       title: 'e2e | Dashboard',
+  //       id: req.session.passport.user.id,
+  //       username: req.session.passport.user.username,
+  //       firstname: req.session.passport.user.firstname,
+  //       lastname: req.session.passport.user.lastname,
+  //       fullname: req.session.passport.user.firstname + " " + req.session.passport.user.lastname,
+  //       email: req.session.passport.user.email
+  //       });
+  //   })
+  //   .post((req, res, next) => {
+  //     // create edit trip route
+  //   })
 
 module.exports = router;
