4ea0cb5d149acb637f23802cb6aebac3668d4438
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-08-05 22:36:05 -0600

    like OMG, the map location list totally like works

--- public/js/plugins/google-map/google-map-script.js
+++ public/js/plugins/google-map/google-map-script.js
@@ -2,83 +2,37 @@ $(function() {
       console.log($('.map-canvas'))
       var mapCards = $('.map-canvas')
 
-      var createGoogleMap = (map) => {
-        var lat = map.attributes[0].toString();
-        var lng = map.attributes[1]
-        console.log(lat,lng);
+      var createGoogleMap = (mapNode) => {
+        // lat, lng logic
+        var lat = parseFloat(mapNode.attr('data-lat'))
+        var lng = parseFloat(mapNode.attr('data-lng'))
+        console.log(lat, lng)
+        var latlng = new google.maps.LatLng(lat, lng)
+        // map settings
+        var settings = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            scrollwheel: false,
+            draggable: true,
+            styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}],
+            mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+            navigationControl: false,
+            navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+        }
+        // declare map
+        var map = new google.maps.Map(document.getElementById(mapNode.attr('id')), settings)
+        console.log(map)
+        // resize map logic
+        google.maps.event.addDomListener(window, "resize", function() {
+            var center = map.getCenter();
+            google.maps.event.trigger(map, "resize");
+            map.setCenter(center);
+            $('#map-canvas').removeClass('loading');
+        });
 
-        // var latlng = new google.maps.latlng()
       }
+      mapCards.each((i, mapNode) => createGoogleMap($(mapNode)))
 
-      $.each(mapCards, (i) => createGoogleMap(mapCards[i]))
-
-
-      // Google Maps
-      $('#map-canvas').addClass('loading');
-      $('.map-canvas').addClass('loading');
-      var latlng = new google.maps.LatLng(40.6700, -73.9400); // Set your Lat. Log. New York
-      var settings = {
-          zoom: 10,
-          center: latlng,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          mapTypeControl: false,
-          scrollwheel: false,
-          draggable: true,
-          styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}],
-          mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-          navigationControl: false,
-          navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-      };
-      var map = new google.maps.Map(document.getElementById("map-canvas2"), settings);
-      var map2 = new google.maps.Map(document.getElementById("map-canvas"), settings);
-
-      google.maps.event.addDomListener(window, "resize", function() {
-          var center = map.getCenter();
-          google.maps.event.trigger(map, "resize");
-          map.setCenter(center);
-          $('#map-canvas').removeClass('loading');
-      });
-
-      console.log('woot')
-      google.maps.event.addDomListener(window, "resize", function() {
-          var center = map2.getCenter();
-          google.maps.event.trigger(map2, "resize");
-          map2.setCenter(center);
-          $('#map-canvas').removeClass('loading');
-      });
-
-      var contentString =
-          '<div id="info-window">'+
-          '<p>18 McLuice Road, Vellyon Hills,<br /> New York, NY 10010<br /><a href="https://plus.google.com/102896039836143247306/about?gl=za&hl=en" target="_blank">Get directions</a></p>'+
-          '</div>';
-      var infowindow = new google.maps.InfoWindow({
-          content: contentString
-      });
-
-      var companyImage = new google.maps.MarkerImage('images/map-marker.png',
-          new google.maps.Size(36,62),// Width and height of the marker
-          new google.maps.Point(0,0),
-          new google.maps.Point(18,52)// Position of the marker
-      );
-
-      var companyPos = new google.maps.LatLng(40.6700, -73.9400);
-
-      var companyMarker = new google.maps.Marker({
-          position: companyPos,
-          map: map,
-          icon: companyImage,
-          title:"Shapeshift Interactive",
-          zIndex: 3});
-
-      var companyMarker = new google.maps.Marker({
-          position: companyPos,
-          map: map2,
-          icon: companyImage,
-          title:"Shapeshift Interactive",
-          zIndex: 3});
-
-      google.maps.event.addListener(companyMarker, 'click', function() {
-          infowindow.open(map,companyMarker);
-          pageView('/#address');
-      });
-    });
+})
