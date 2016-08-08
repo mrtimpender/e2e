6f9da90ded7815fcafdd3196ed208c77d0f14db2
Jonathan Huxhold <jonathanhuxhold@Jonathans-MacBook-Pro.local>
2016-08-07 18:30:58 -0600

    new trip route and query

--- controllers/database/trips/tripQueries.js
+++ controllers/database/trips/tripQueries.js
@@ -10,13 +10,13 @@ module.exports = {
   getTripById: (id) => {
     return db.knex('user_trips').where('id', id)
   },
-  getTripTransitMethod: (id) => {
-    return db.knex('transit_methods').select('transit_type').where('id', id)
+  getTripTransitMethod: (mode) => {
+    return db.knex('transit_methods').select('id').where('transit_type', mode)
   },
   getTripsAndLocations: () => {
     return db.knex.raw(`select t.*, l.name as origin_name, l2.name as destination_name from user_trips t
                     join user_locations l on t.origin_loc_id = l.id
                     join user_locations l2 on t.destination_loc_id = l2.id`)
-  },
+  }
 
 }
